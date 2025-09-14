46eeef422b4a128b0809ad2976b1441244503c2a
clarenzmauro <m27oflegend@gmail.com>
2025-09-14 08:37:34 +0800

    installed expo and used its router

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Hello World!</Text>
+        </View>
+    );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
