823e1f2ab350a38a3224b9f042ce95d0e7285480
clarenzmauro <m27oflegend@gmail.com>
2025-09-14 09:07:20 +0800

    view, text, image components

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import logo from "../assets/me.png";
 
 const Home = () => {
-    return (
-        <View>
-            <Text>Hello World!</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+
+      <Text style={styles.title}>Hello World!</Text>
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>
+        First react native app
+      </Text>
+
+      <View style={styles.card}>
+        <Text>This is a card</Text>
+      </View>
+    </View>
+  );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  card: {
+    backgroundColor: "#eee",
+    padding: 20,
+    borderRadius: 5,
+    boxShadow: "4px 4px rgba(0, 0, 0, 0.1)",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginVertical: 20,
+  },
+});
---
+++ assets.d.ts
@@ -0,0 +1,1 @@
+declare module "*.png" { const value: number; export default value; }
